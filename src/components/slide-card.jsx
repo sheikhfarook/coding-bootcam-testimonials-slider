350d72a273964df47907710fd5ad5783ff813036
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-02-19 12:18:46 +0530

    update the slide care

--- src/components/slide-card.jsx
+++ src/components/slide-card.jsx
@@ -1,5 +1,34 @@
+import antya from "../assets/image-tanya.jpg";
+import john from "../assets/image-john.jpg";
+import preview from "../assets/icon-prev.svg";
+import next from "../assets/icon-next.svg";
 const SlideCard = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+          &quot;I&quot;ve been interested in coding for a while but never taken
+          the jump , until now. I couldn&quot;t recommend this course
+          enough.I&quot;m now in the job of my dreams and so ecited about the
+          furture.&quot;
+        </p>
+        <p>
+          Tanya Sinclair <span>UX Engineer</span>
+        </p>
+      </div>
+      <div>
+        <img className="" src={antya} alt="" />
+        <img src={john} alt="" />
+        <div>
+          <button className="">
+            <img src={preview} alt="" />
+          </button>
+          <button>
+            <img src={next} alt="" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SlideCard;
