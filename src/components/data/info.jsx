ddec54a61f679d0b02db993505ba7806406efccd
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-02-19 14:21:54 +0530

    i create data

--- src/components/data/info.jsx
+++ src/components/data/info.jsx
@@ -1,1 +1,16 @@
+export const data = [
+  {
+    id: 1,
+    info: "&quot; I&apos;ve been interested in coding for a while but never taken the jump, until now. I couldn&apos;t recommend this course enough. I&apos;m now in the job of my dreams and so excited aboutthe furture.&quot;",
+    name: "Tanya Sinclair",
     roll: "UX Engineer",
+    images: "Tanya",
+  },
+  {
+    id: 2,
+    info: "&quot; If you want to lay the best foundation possible i&apos;d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer.&quot;",
+    name: "John TaKpor ",
+    roll: "Junior Front-end Developer",
+    images: "john",
+  },
+];
